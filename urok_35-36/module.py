21e52ce9f00fb8f8aa5f9543cf0fcc71756219ab
pokakaite <danilova.katherine02@gmail.com>
2024-05-20 19:16:51 +0500

    классная работа

---
+++ urok_35-36/module.py
@@ -0,0 +1,18 @@
+class Calculator:
+    def add(self, x1, x2):
+        return x1 + x2
+
+    def mul(self, x1, x2):
+        return x1 * x2
+
+    def div(self, x1, x2):
+        return x1 / x2
+
+    def subtract(self, x1, x2):
+        return x1 - x2
+
+
+print(__name__)
+if __name__ == '__main__':
+    print('module')
+
